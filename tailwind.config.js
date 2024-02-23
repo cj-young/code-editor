bd817cd92a9be819040fcd47d3b5b2a5e44befe1
CJ Young <chrjyoung@gmail.com>
2024-02-23 00:00:39 -0500

    💄 Add tailwind color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "rgb(var(--text))",
+        background: "rgb(var(--background))",
+        primary: "rgb(var(--primary))",
+        secondary: "rgb(var(--secondary))",
+        accent: "rgb(var(--accent))",
+      },
+    },
   },
   plugins: [],
 };
