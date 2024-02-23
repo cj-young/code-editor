4511718cc2ac0d685bcd4d51749a28b1b8a98a33
CJ Young <chrjyoung@gmail.com>
2024-02-23 00:00:22 -0500

    🐛 Fix incorrect import path for home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/feature/home/home/home.component';
+import { HomeComponent } from './home/feature/home/home.component';
 
 export const routes: Routes = [
   {
