b92f52062a8c1b37ac275f3cbef8dbc9cfc5e32f
CJ Young <chrjyoung@gmail.com>
2024-02-23 23:40:53 -0500

    ✨ Add theme toggle button to home navbar

--- src/app/home/feature/home-navbar/home-navbar.component.ts
+++ src/app/home/feature/home-navbar/home-navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogoLinkComponent } from '../../../shared/feature/logo-link/logo-link.component';
+import { ThemeToggleComponent } from '../../../shared/feature/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-home-navbar',
   standalone: true,
-  imports: [LogoLinkComponent],
+  imports: [LogoLinkComponent, ThemeToggleComponent],
   templateUrl: './home-navbar.component.html',
   styles: ``,
 })
