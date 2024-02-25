9ab3816ce7e59c0e61386e31d47accee19d2c6c3
CJ Young <chrjyoung@gmail.com>
2024-02-25 14:49:56 -0500

    🚚 Move CodeMirror imports to separate file

---
+++ src/config/codemirror-imports.ts
@@ -0,0 +1,8 @@
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/scroll/simplescrollbars';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/htmlembedded/htmlembedded';
+import 'codemirror/mode/javascript/javascript';
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import 'codemirror/addon/comment/comment';
-import 'codemirror/addon/edit/closebrackets';
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/matchbrackets';
-import 'codemirror/addon/scroll/simplescrollbars';
-import 'codemirror/mode/css/css';
-import 'codemirror/mode/htmlembedded/htmlembedded';
-import 'codemirror/mode/javascript/javascript';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import './config/codemirror-imports';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
