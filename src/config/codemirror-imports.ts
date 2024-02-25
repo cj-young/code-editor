4e1b698260b9755e1fda8175cf7061250dc0ea24
CJ Young <chrjyoung@gmail.com>
2024-02-25 14:50:38 -0500

    💡 Add comments to CodeMirror imports

--- src/config/codemirror-imports.ts
+++ src/config/codemirror-imports.ts
@@ -1,8 +1,11 @@
+// Addons
 import 'codemirror/addon/comment/comment';
 import 'codemirror/addon/edit/closebrackets';
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/matchbrackets';
 import 'codemirror/addon/scroll/simplescrollbars';
+
+// Modes
 import 'codemirror/mode/css/css';
 import 'codemirror/mode/htmlembedded/htmlembedded';
 import 'codemirror/mode/javascript/javascript';
