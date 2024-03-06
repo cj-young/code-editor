10bb8765fac874cb15333cef50aa5d2cde79cbe3
CJ Young <chrjyoung@gmail.com>
2024-03-05 23:36:47 -0500

    ✨ Add Spark to local storage on save

--- src/app/editor/feature/save-modal/save-modal.component.ts
+++ src/app/editor/feature/save-modal/save-modal.component.ts
@@ -1,8 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  Storage,
+  getDownloadURL,
+  ref,
+  uploadString,
+} from '@angular/fire/storage';
 import { FormsModule } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+import { LocalStorageService } from '../../../shared/feature/local-storage-service/local-storage.service';
 import { ModalService } from '../../../shared/feature/modal-service/modal.service';
 import { XmarkSvgComponent } from '../../../shared/ui/xmark-svg/xmark-svg.component';
 import { EditorScreenshotService } from '../editor-screenshot/editor-screenshot.service';
+import { EditorService } from '../editor-service/editor.service';
 
 @Component({
   selector: 'app-save-modal',
@@ -18,17 +28,20 @@ import { EditorScreenshotService } from '../editor-screenshot/editor-screenshot.
 export class SaveModalComponent implements OnInit {
   sparkName = '';
   isLoading = false;
-  screenshotDataUrl = '';
+  screenshotDataUrl = new BehaviorSubject<string | undefined>(undefined);
   @Input() screenshotPromise?: Promise<string>;
   constructor(
     private modalService: ModalService,
-    private editorScreenshotService: EditorScreenshotService
+    private editorScreenshotService: EditorScreenshotService,
+    private fbStorage: Storage,
+    private localStorageService: LocalStorageService,
+    private editorService: EditorService
   ) {}
 
   async ngOnInit(): Promise<void> {
     const dataUrl =
       (await this.editorScreenshotService.getScreenShot()) as string;
-    this.screenshotDataUrl = dataUrl;
+    this.screenshotDataUrl.next(dataUrl);
   }
 
   onClose() {
@@ -37,6 +50,25 @@ export class SaveModalComponent implements OnInit {
 
   onSubmit(e: SubmitEvent) {
     e.preventDefault();
-    console.log(this.screenshotDataUrl);
+    this.isLoading = true;
+    this.screenshotDataUrl.subscribe(async (dataUrl) => {
+      try {
+        if (!dataUrl) return;
+        const sparkId = uuidv4();
+        const storageRef = ref(this.fbStorage, `thumbnails/saved/${sparkId}`);
+        const snapshot = await uploadString(storageRef, dataUrl, 'data_url');
+        const url = await getDownloadURL(snapshot.ref);
+
+        this.localStorageService.addSpark(
+          this.editorService.inputCode,
+          this.sparkName,
+          sparkId,
+          url
+        );
+      } catch (error) {
+        console.error(error);
+        this.isLoading = false;
+      }
+    });
   }
 }
