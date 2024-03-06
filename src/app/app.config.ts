86138d7594eefac129328909cdf465524e427557
CJ Young <chrjyoung@gmail.com>
2024-03-05 23:34:56 -0500

    ➕ Add Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase))
+    ),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
 };
