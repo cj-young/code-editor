8d62575ea4efbccbb58bc9689e087cae35e1c167
CJ Young <chrjyoung@gmail.com>
2024-03-09 18:45:39 -0500

    ✨ Add sort selection dropdown to home page

--- src/app/home/feature/home/home.component.ts
+++ src/app/home/feature/home/home.component.ts
@@ -3,9 +3,15 @@ import {
   LocalStorageService,
   PersonalSpark,
 } from '../../../shared/feature/local-storage-service/local-storage.service';
+import {
+  DropdownOption,
+  DropdownSelectComponent,
+} from '../../../shared/ui/dropdown-select/dropdown-select.component';
 import { NewSparkButtonComponent } from '../../ui/new-spark-button/new-spark-button.component';
 import { SparkPreviewComponent } from '../../ui/spark-preview/spark-preview.component';
 import { HomeNavbarComponent } from '../home-navbar/home-navbar.component';
+
+type GallerySort = 'popular' | 'newest' | 'oldest';
 
 @Component({
   selector: 'app-home',
@@ -14,15 +20,26 @@ import { HomeNavbarComponent } from '../home-navbar/home-navbar.component';
     HomeNavbarComponent,
     NewSparkButtonComponent,
     SparkPreviewComponent,
+    DropdownSelectComponent,
   ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
   personalSparks: PersonalSpark[] = [];
+  gallerySort: GallerySort = 'popular';
+  sortDropdownOptions: DropdownOption<string, GallerySort>[] = [
+    { displayName: 'Popular', id: 'popular' },
+    { displayName: 'Newest', id: 'newest' },
+    { displayName: 'Oldest', id: 'oldest' },
+  ];
 
   constructor(private localStorageService: LocalStorageService) {}
 
   ngOnInit(): void {
     this.personalSparks = this.localStorageService.getPersonalSparks();
+  }
+
+  onSortChange(newSort: GallerySort) {
+    this.gallerySort = newSort;
   }
 }
