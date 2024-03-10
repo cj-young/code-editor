c4567005d3382d68e2cffb35019ece9df2c890cc
CJ Young <chrjyoung@gmail.com>
2024-03-10 12:57:46 -0400

    ✨ Add share button to saved Spark editor

--- src/app/editor/feature/saved-code-editor/feature/saved-navbar/saved-navbar.component.ts
+++ src/app/editor/feature/saved-code-editor/feature/saved-navbar/saved-navbar.component.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 import { LocalStorageService } from '../../../../../shared/feature/local-storage-service/local-storage.service';
 import { LogoLinkComponent } from '../../../../../shared/feature/logo-link/logo-link.component';
+import { ModalService } from '../../../../../shared/feature/modal-service/modal.service';
 import { ThemeToggleComponent } from '../../../../../shared/feature/theme-toggle/theme-toggle.component';
 import { FloppyDiskSvgComponent } from '../../../../../shared/ui/floppy-disk-svg/floppy-disk-svg.component';
+import { ShareSvgComponent } from '../../../../../shared/ui/share-svg/share-svg.component';
 import { EditorService } from '../../../shared/data-access/editor-service/editor.service';
 import { EditorScreenshotService } from '../../../shared/feature/editor-screenshot/editor-screenshot.service';
+import { ShareModalComponent } from '../../../shared/feature/share-modal/share-modal.component';
 
 @Component({
   selector: 'app-saved-navbar',
   standalone: true,
-  imports: [LogoLinkComponent, FloppyDiskSvgComponent, ThemeToggleComponent],
+  imports: [
+    LogoLinkComponent,
+    FloppyDiskSvgComponent,
+    ThemeToggleComponent,
+    ShareSvgComponent,
+  ],
   templateUrl: './saved-navbar.component.html',
   styles: `
     :host {
@@ -24,7 +32,8 @@ export class SavedNavbarComponent implements OnInit {
   constructor(
     private localStorageService: LocalStorageService,
     private editorScreenshotService: EditorScreenshotService,
-    private editorService: EditorService
+    private editorService: EditorService,
+    private modalService: ModalService
   ) {}
 
   ngOnInit(): void {
@@ -51,5 +60,9 @@ export class SavedNavbarComponent implements OnInit {
       sparkId,
       imageUrl
     );
+  }
+
+  onShareClick() {
+    this.modalService.openModal(ShareModalComponent);
   }
 }
