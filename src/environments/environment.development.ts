4f73dc5b56a0e30bad07953b9748577b3c4c5ada
CJ Young <chrjyoung@gmail.com>
2024-03-10 18:06:52 -0400

    🔧 Fix Firebase config not working in dev environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'devforge-eb84a',
+    appId: '1:216213062429:web:c58e8548a96fa838c417f4',
+    storageBucket: 'devforge-eb84a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAt1KZXx7izB454hJ0JDDm4i_3OMfZsAng',
+    authDomain: 'devforge-eb84a.firebaseapp.com',
+    messagingSenderId: '216213062429',
+    measurementId: 'G-TTDYLLCH4R',
+  },
+};
