ef7093b72a8c4e7d6558988a6fb31d8ddb11e25a
CJ Young <chrjyoung@gmail.com>
2024-03-30 02:15:50 -0400

    ✨ Add color theme to local storage

--- src/app/shared/feature/theme-service/theme.service.ts
+++ src/app/shared/feature/theme-service/theme.service.ts
@@ -2,12 +2,25 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Theme } from '../../types/theme';
 
+const LOCAL_STORAGE_THEME_KEY = 'theme';
+const DEFAULT_THEME: Theme = 'dark';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
-  private currentTheme = new BehaviorSubject<Theme>('dark');
+  private currentTheme = new BehaviorSubject<Theme>(this.loadTheme());
   currentTheme$ = this.currentTheme.asObservable();
+
+  private loadTheme(): Theme {
+    const storedTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY);
+    if (storedTheme !== 'light' && storedTheme !== 'dark') return DEFAULT_THEME;
+    return storedTheme;
+  }
+
+  private saveTheme(theme: Theme) {
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+  }
 
   getTheme(): Theme {
     return this.currentTheme.getValue();
@@ -15,5 +28,6 @@ export class ThemeService {
 
   setTheme(newTheme: Theme) {
     this.currentTheme.next(newTheme);
+    this.saveTheme(newTheme);
   }
 }
